960c7099ec6d0fb1aa5ef5b5694a0bb4d1a085a1
Alex Trost <19617280+a-trost@users.noreply.github.com>
2025-05-04 20:30:49 -0400

    Add prismic preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,15 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import localFont from "next/font/local";
 
-import "./globals.css";
-import { Footer } from "@/components/Footer";
-import { NavBar } from "@/components/NavBar";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
 import { createClient } from "@/prismicio";
 import { isFilled } from "@prismicio/client";
 import { ViewTransitions } from "next-view-transitions";
+
+import { NavBar } from "@/components/NavBar";
+import { Footer } from "@/components/Footer";
+import "./globals.css";
 
 const raleway = Raleway({
   variable: "--font-raleway",
@@ -57,6 +60,7 @@ export default async function RootLayout({
           <main className="pt-14 md:pt-16">{children}</main>
           <Footer />
         </body>
+        <PrismicPreview repositoryName={repositoryName} />
       </html>
     </ViewTransitions>
   );
